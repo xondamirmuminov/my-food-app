feac1b8800a53c73f462d7f0b054bea95a6f640b
Alisher778 <alisher.musurmonov89@gmail.com>
2021-06-22 10:20:34 +0500

    :sparkles: Added usestate

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import Home from './containers/Home';
 import Login from './containers/Auth/Login';
 import SignUp from './containers/Auth/SignUp';
 import Header from './containers/Sidebar/Header';
+import PageNotFound from './components/NotFoundPage';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/sign-up" component={SignUp} />
+        <Route component={PageNotFound} />
       </Switch>
     </div>
   );
--- src/containers/Sidebar/Header.js
+++ src/containers/Sidebar/Header.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './style.css';
 
 export default function Header() {
   return (
     <div>
       <h3>Header</h3>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/sign-up">Sign-Up</Link></li>
+        <li>
+          <NavLink
+            to="/"
+            exact={true}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            exact
+            to="/login"
+          >Login</NavLink></li>
+        <li>
+          <NavLink
+            exact
+            to="/sign-up"
+          >Sign-Up</NavLink></li>
       </ul>
     </div>
   )
